e8d15b1789ae3fa3d3b980febf7b629ec6fda9fd
Parjad Minooei <minooeip@gmail.com>
2025-08-08 12:50:42 -0400

    first commit

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -2,8 +2,18 @@
 @section('content')
     <h1>Add a Student</h1>
     <form action="{{route('students.store')}}" method="POST">
+        @if($errors->any())
+            @foreach ($errors->all() as $error)
+                <span style="color:red">{{ $error }}</span><br>
+            @endforeach
+        @endif
         {{csrf_field() }}
         <input type="text" name="name" placeholder="Name">
+        <br>
+            @error('name')
+                <span style="color:red">{{ $message }}</span><br>
+            @enderror
+        <br>
         <input type="email" name="email" placeholder="Email">
         <button type="submit">Submit</button>
     </form>
